2fa3472fc4e588f06e320bb262e22214413908b3
hykim <hiyoung.kimmy@gmail.com>
2024-01-13 10:56:18 -0800

    change hostname

--- server/knexfile.js
+++ server/knexfile.js
@@ -6,5 +6,10 @@ require('dotenv').config();
  */
 module.exports = {
   client: 'mysql2',
-  connection: process.env.CLEARDB_DATABASE_URL,
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  }
 };
