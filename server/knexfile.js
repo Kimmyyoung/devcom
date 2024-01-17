15f60e51381839717e6f1de3bad76f1a5e27d114
hykim <hiyoung.kimmy@gmail.com>
2024-01-17 13:47:05 -0800

    server heroku

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,5 @@
 require('dotenv').config();
-// Update with your config settings.
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
   client: 'mysql2',
   connection: {
